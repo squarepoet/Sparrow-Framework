4d84ecb0b56ed8baa348118002cf4ea0f714a94c
Daniel Sperl <daniel@gamua.com>
2014-07-04 09:07:38 +0200

    updated some comments

--- samples/barebone/src/Classes/Game.m
+++ samples/barebone/src/Classes/Game.m
@@ -3,7 +3,7 @@
 //  AppScaffold
 //
 
-#import "Game.h" 
+#import "Game.h"
 
 @implementation Game
 
@@ -13,24 +13,25 @@
     {
         // This is where the code of your game will start;
         // in this sample, we add just a simple quad to see if it works.
-        
+
         SPQuad *quad = [SPQuad quadWithWidth:100 height:100];
         quad.color = 0xff0000;
         quad.x = 50;
         quad.y = 50;
         [self addChild:quad];
-        
-        
+
+
         // Per default, this project compiles as an universal application. To change that, enter the
-        // project info screen, and in the "Build"-tab, find the setting "Targeted device family".
+        // project info screen, and in the "General"-tab, find the setting "Deployment Info".
         //
-        // Now Choose:  
+        // Now choose the right setting in the "Devices" dropdown:
+        //
         //   * iPhone      -> iPhone only App
         //   * iPad        -> iPad only App
-        //   * iPhone/iPad -> Universal App  
-        // 
-        // The "iOS deployment target" setting must be at least "iOS 5.0" for Sparrow 2.
-        // Always used the latest available version as the base SDK.
+        //   * iPhone/iPad -> Universal App
+        //
+        // The "Deployment target" setting must be at least "iOS 5.0" for Sparrow 2.
+        // Always use the latest available version as the base SDK.
     }
     return self;
 }
