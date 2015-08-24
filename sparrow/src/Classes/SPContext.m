1e1430a9debf13fa06e381d3db312d297109102d
Robert Carone <racarone@gmail.com>
2015-08-24 12:32:37 -0700

    enable multithreading on context, if available

--- sparrow/src/Classes/SPContext.m
+++ sparrow/src/Classes/SPContext.m
@@ -167,6 +167,9 @@ static SPRenderingAPI toSPRenderingAPI[] = {
         {
             contexts[nativeContext] = self;
             _nativeContext = [nativeContext retain];
+            
+            if ([nativeContext respondsToSelector:@selector(setMultiThreaded:)])
+                _nativeContext.multiThreaded = YES;
         }
         else
         {
