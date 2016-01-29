e99e9357956f8abccf862270d2a7195174d07761
Robert Carone <racarone@gmail.com>
2016-01-29 15:51:29 +0000

    change `demo_swift` to use a storyboard

--- samples/demo_swift/src/Classes/DemoAppDelegate.swift
+++ samples/demo_swift/src/Classes/DemoAppDelegate.swift
@@ -18,34 +18,16 @@ let CENTER_Y: Float = GAME_HEIGHT / 2.0
 
 @UIApplicationMain
 class DemoAppDelegate: UIResponder, UIApplicationDelegate {
-    var _viewController: SPViewController!
-    var _window: UIWindow!
+    @IBOutlet var viewController: SPViewController?
+    var window: UIWindow?
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        
         NSSetUncaughtExceptionHandler() { exception in
             print("uncaught exception: \(exception.description)")
         }
         
-        _window = UIWindow(frame: UIScreen.mainScreen().bounds)
-        
         SPAudioEngine.start()
-        
-        _viewController = SPViewController()
-        _viewController.multitouchEnabled = true
-        _viewController.preferredFramesPerSecond = 60
-        _viewController.startWithRoot(Game.self, supportHighResolutions: true, doubleOnPad: true)
-        
-        _window.rootViewController = _viewController
-        _window.makeKeyAndVisible()
-        
-        // What follows is a very simple approach to support the iPad:
-        // we just center the stage on the screen!
-        //
-        // (Beware: to support autorotation, this would need a little more work.)
-        
-        if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
-            _viewController.view.frame = CGRectMake(64, 32, 640, 960)
-        }
         
         return true
     }
---
+++ samples/demo_swift/src/Classes/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Robert Carone on 1/24/16.
+//
+//
+
+import UIKit
+import Sparrow
+
+class ViewController: SPViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        multitouchEnabled = true
+        preferredFramesPerSecond = 60
+        startWithRoot(Game.self, supportHighResolutions: true, doubleOnPad: true)
+        
+        // What follows is a very simple approach to support the iPad:
+        // we just center the stage on the screen!
+        //
+        // (Beware: to support autorotation, this would need a little more work.)
+        
+        if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
+            view.frame = CGRectMake(64, 32, 640, 960)
+        }
+    }
+}
